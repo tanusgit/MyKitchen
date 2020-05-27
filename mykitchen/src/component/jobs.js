22efd860d91a512a0f77457e8b86529eb71d6a67
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-26 18:50:43 -0700

    added navabr

--- mykitchen/src/component/jobs.js
+++ mykitchen/src/component/jobs.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 class Job extends Component {
 
@@ -13,8 +15,43 @@ class Job extends Component {
 
 
         return (
+
             <div className="container" style={{ marginleft: '25px', marginright: '25px', bordercolor: 'blue' }}>
-                <div id="co">
+                <div className="row">
+                    <div className="col-md-5 m-2">
+                        <Card>
+                            <CardImg src="https://images.pexels.com/photos/461077/pexels-photo-461077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                            <CardBody>
+                                <CardTitle>Card title</CardTitle>
+                                <CardSubtitle>Card subtitle</CardSubtitle>
+                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                <Button href="https://reactstrap.github.io/components/card/">Button</Button>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-md-5 m-2">
+                        <Card>
+                            <CardImg src="https://images.pexels.com/photos/461077/pexels-photo-461077.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                            <CardBody>
+                                <CardTitle>Card title</CardTitle>
+                                <CardSubtitle>Card subtitle</CardSubtitle>
+                                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                                <Button href="https://reactstrap.github.io/components/card/">Button</Button>
+                            </CardBody>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+export default Job;
+
+
+
+/*<div className="container" style={{ marginleft: '25px', marginright: '25px', bordercolor: 'blue' }}>
+                    <div id="co">
                     <aside>
                         <p>Your job post is here: </p>
                     </aside>
@@ -33,12 +70,5 @@ class Job extends Component {
                         </p>
                     </section>
                 </div>
-            </div>
-
-        );
-    }
-}
-export default Job;
-
-
-
+                </div>
+*/
